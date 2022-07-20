ecce650903b4b478aed3fbd8125e4c55bf44a94f
Brendan Allan <brendonovich@outlook.com>
2022-07-21 06:33:30 +0800

    alt to move lines

--- nvim/lua/brendonovich/set.lua
+++ nvim/lua/brendonovich/set.lua
@@ -6,3 +6,12 @@ vim.opt.expandtab = false
 vim.opt.wrap = false
 vim.opt.updatetime = 50
 vim.opt.relativenumber = true
+
+-- Move lines up + down with Alt
+vim.keymap.set('n', '∆', ':m +1<cr>')
+vim.keymap.set('n', '˚', ':m -2<cr>')
+vim.keymap.set('i', '∆', '<esc>:m +1<cr>==gi')
+vim.keymap.set('i', '˚', '<esc>:m -2<cr>==gi')
+vim.keymap.set('v', '∆', ":m '>+1<cr>gv=gv")
+vim.keymap.set('v', '˚', ":m '<-2<cr>gv=gv")
+
