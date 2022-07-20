a51565f9e4b0c285badd4c8dabd6c3ffcdb84c97
Brendan Allan <brendonovich@outlook.com>
2022-07-20 00:28:59 -0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+if ! sudo -v; then
+  echo "Superuser not granted, aborting installation"
+  exit 1
+fi
+
+REPO_URL='https://github.com/brendonovich/.dotfiles'
+DOWNLOAD_PATH='/tmp/.dotfiles'
+INSTALL_PATH='~/.dotfiles'
+
+curl -L -o $DOWNLOAD_PATH $REPO_URL/tarball/master
+
+mkdir -p $INSTALL_PATH
+sudo tar -xzof $DOWNLOAD_PATH -C $INSTALL_PATH --strip-components=1
+
+$INSTALL_PATH/setup.sh
